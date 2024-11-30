811dfbc72bf146427fd147cd05c15f1d492d8786
Muhammad Hasan Haikal <muhammadhasan.genju@gmail.com>
2024-11-30 14:17:01 +0700

    Update massage.js

--- massage.js
+++ massage.js
@@ -58,7 +58,7 @@ window.onload = function() {
         </style>
         <div class="welcome-overlay">
             <div class="welcome-box">
-                <img src="img/Hasan 1.jpg" alt="Welcome">
+                <img src="https://muhammad-hasan-web-developer.github.io/img/Hasan 1.jpg" alt="Welcome">
                 <h2>Welcome to My Portfolio!</h2>
                 <p>✨ Let's explore my portfolio together ✨</p>
                 <button onclick="this.parentElement.parentElement.remove()">Get Started!</button>
